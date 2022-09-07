d666812ab12b9eb1079a47950106a79738c86e13
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-07 22:22:13 +0300

    add review form

--- src/Components/NewReview.js
+++ src/Components/NewReview.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function NewReview() {
+function NewReview({onAddReview}) {
     const [body, setBody] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        fetch("http://localhost:9292/reviews", {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                body: body
+            }),
+        })
+            .then((response) => response.json)
+            .then((newReview) => {
+                onAddReview(newReview);
+                setBody("")
+            });
+    }
   return (
-    <div>NewReview</div>
-  )
+    <div>
+        <form className='review-form' onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            name="body"
+            autoComplete='off'
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            />
+            <button type={submit}>Send Review</button>
+        </form>
+    </div>
+  );
 }
 
 export default NewReview
