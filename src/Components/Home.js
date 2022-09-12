829490900df260ee12b48f7c2b0b3b36cce8e3dd
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-12 15:56:32 +0300

    add services to home section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,13 +16,38 @@ function Home() {
           <img src={hotel} alt="hotel1" className="hotel1" style={{width: "850px", height: "600px", marginLeft: "3rem"}}/>
         </div>
         </div>
+        <div>
+          <h2 style={{color: "green", marginTop: "3rem"}}>Our Services</h2>
+        </div>
+        <div>
+          <div>
+            <h4>High Speed Internet Access</h4>
+            <p>Stay connected with the world, Wifi <br></br>access in the hotel allows you to browse<br></br> in peace. Whether for your professional <br></br>or personal interests, we are providing <br></br>this service free of charge.</p>
+          </div>
+          <div>
+            <h4>Laundry</h4>
+            <p>Laundry and Ironing: Services that are <br></br>offered every day of the week. Items <br></br>received before 9:00 will be given on the <br></br>same day. If you need ironing emergency, <br></br>we can do it for you quickly between 8:00 <br></br>a.m to 5:00 p.m. Our staff is at your <br></br>disposal for your needs.</p>
+          </div>
+          <div>
+            <h4>Breakfast</h4>
+            <p>Breakfast is available daily from 06: 00 to <br></br>08: 30 am, outside of these hour.</p>
+          </div>
+          <div>
+            <h4>Massage</h4>
+            <p>We offer two massage rooms located on <br></br>the 4th floor of the hotel. The expert <br></br>hands of our masseuses will invite you <br></br>to total relaxation.</p>
+          </div>
+          <div>
+            <h4>Guarded parking Lot</h4>
+            <p></p>
+          </div>
+        </div>
           <div className='footer'>
             <h3>
               My Footer
             </h3>
           </div>
           <div>
-            <p className='btm-footer' style={{}}>Copyright@ReelWorldFurnitures. All rights reserved.</p>
+            <p className='btm-footer' style={{textAlign:"center", fontWeight: "700"}}>Copyright@ReelWorldFurnitures. All rights reserved.</p>
           </div>
     </div>
     )
