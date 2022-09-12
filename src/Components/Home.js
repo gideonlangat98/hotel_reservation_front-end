2d1513a06f0d719f83800fec6f6556b457c159c0
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-12 16:20:41 +0300

    style service section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,33 +13,27 @@ function Home() {
           <p>Welcome to ReelWorld. <br></br>ReelWorld hotel 5 minutes by car from Nairobi CBD. <br></br>it offers free WIFI and parking.</p>
         </div>
         <div>
-          <img src={hotel} alt="hotel1" className="hotel1" style={{width: "850px", height: "600px", marginLeft: "3rem"}}/>
+          <img src={hotel} alt="hotel1" className="hotel1" style={{width: "750px", height: "600px", marginLeft: "3rem"}}/>
         </div>
         </div>
         <div>
-          <h2 style={{color: "green", marginTop: "3rem"}}>Our Services</h2>
+          <h2 style={{color: "green", marginTop: "3rem", marginLeft: "2.5rem"}}>Our Services</h2>
         </div>
-        <div>
-          <div>
+        <div style={{display: "flex", marginLeft: "2.5rem", marginTop: "1rem"}}>
+          <div className='service1'>
             <h4>High Speed Internet Access</h4>
             <p>Stay connected with the world, Wifi <br></br>access in the hotel allows you to browse<br></br> in peace. Whether for your professional <br></br>or personal interests, we are providing <br></br>this service free of charge.</p>
           </div>
-          <div>
+          <div className='service2' style={{paddingLeft: "3rem"}}>
             <h4>Laundry</h4>
             <p>Laundry and Ironing: Services that are <br></br>offered every day of the week. Items <br></br>received before 9:00 will be given on the <br></br>same day. If you need ironing emergency, <br></br>we can do it for you quickly between 8:00 <br></br>a.m to 5:00 p.m. Our staff is at your <br></br>disposal for your needs.</p>
           </div>
-          <div>
+          <div className='service3' style={{paddingLeft: "3rem"}}>
             <h4>Breakfast</h4>
             <p>Breakfast is available daily from 06: 00 to <br></br>08: 30 am, outside of these hour.</p>
           </div>
-          <div>
-            <h4>Massage</h4>
-            <p>We offer two massage rooms located on <br></br>the 4th floor of the hotel. The expert <br></br>hands of our masseuses will invite you <br></br>to total relaxation.</p>
           </div>
           <div>
-            <h4>Guarded parking Lot</h4>
-            <p></p>
-          </div>
         </div>
           <div className='footer'>
             <h3>
