6e109012a7f01e9941a2ab67f61a468bad82e386
gideonlangat98 <gideonlangat98@gmail.com>
2022-09-14 15:19:13 +0300

    add readme.md

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,9 @@ import hotel2 from "../images/hotel2.jpg"
 
 
 function Home() {
-    
     return (
-      <div className='bg-page'>
-        <div className='intro'>
+    <div className='bg-page'>
+      <div className='intro'>
         <div className='p-home'>
           <img src={hotel2} alt="hotel2" className='hotel2' style={{width: "400px", height: "350px"}}/>
           <p>Welcome to ReelWorld. <br></br>ReelWorld hotel 5 minutes by car from Nairobi CBD. <br></br>it offers free WIFI and parking.</p>
@@ -23,7 +22,7 @@ function Home() {
           <div className='service1'>
             <h4>High Speed Internet Access</h4>
             <p>Stay connected with the world, Wifi <br></br>access in the hotel allows you to browse<br></br> in peace. Whether for your professional <br></br>or personal interests, we are providing <br></br>this service free of charge.</p>
-          </div>
+            </div>
           <div className='service2' style={{paddingLeft: "3rem"}}>
             <h4>Laundry</h4>
             <p>Laundry and Ironing: Services that are <br></br>offered every day of the week. Items <br></br>received before 9:00 will be given on the <br></br>same day. If you need ironing emergency, <br></br>we can do it for you quickly between 8:00 <br></br>a.m to 5:00 p.m. Our staff is at your <br></br>disposal for your needs.</p>
@@ -31,10 +30,8 @@ function Home() {
           <div className='service3' style={{paddingLeft: "3rem"}}>
             <h4>Breakfast</h4>
             <p>Breakfast is available daily from 06: 00 to <br></br>08: 30 am, outside of these hour.</p>
+            </div>
           </div>
-          </div>
-          <div>
-        </div>
           <div className='footer' style={{display: "flex"}}>
             <div style={{marginRight: "10rem", marginLeft: "4rem"}}>
             <h3>Quick Links</h3>
